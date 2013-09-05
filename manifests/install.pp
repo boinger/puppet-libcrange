d7f66e8aa6fdfccdecf589eb7052630900d7601c
jeff@jeffvier.com <jeff@jeffvier.com>
2013-09-05 15:29:54 -0700

    also install perl_seco_data_range

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,6 +7,9 @@ class libcrange::install (
   $mod_ranged_name     = 'mod_ranged',
   $mod_ranged_provider = 'git',
   $mod_ranged_giturl   = 'https://github.com/boinger/mod_ranged.git',
+  $perl_range_name     = 'perl_seco_data_range',
+  $perl_range_provider = 'git',
+  $perl_range_giturl   = 'https://github.com/boinger/perl_seco_data_range.git',
   )
 {
   if $architecture == "x86_64" {
@@ -181,10 +184,46 @@ class libcrange::install (
         notify  => Service['httpd'],
         require => Exec["apxs ${mod_ranged_name}"];
     }
-
   }
   else {
     notify { "It's up to you to provde mod_ranged": }
+  }
+
+  if $perl_range_provider == 'git' {
+    exec {
+      "git clone ${perl_range_name}":
+        cwd     => $temp_dir,
+        command => "git clone $perl_range_giturl",
+        creates => "${temp_dir}/${perl_range_name}",
+        timeout => 0,
+        path    => ["/usr/bin"],
+        require => [
+          Package['git'],
+          File["${temp_dir}"],
+        ];
+
+      "make ${perl_range_name}":
+        cwd     => "${temp_dir}/${perl_range_name}/source",
+        command => "perl Makefile.PL && make && make test && make install",
+        creates => "/usr/local/share/perl5/Seco/Data/Range.pm",
+        require => Exec["git clone ${perl_range_name}"];
+
+      "install er":
+        cwd     => "${temp_dir}/${perl_range_name}",
+        command => "/usr/bin/install -m 0755 root/usr/bin/er /usr/bin",
+        creates => "/usr/bin/er",
+        require => Exec["git clone ${perl_range_name}"];
+
+      "install er manpage":
+        cwd     => "${temp_dir}/${perl_range_name}",
+        command => "/usr/bin/install -m 0644 root/usr/share/man/man1/er.1 /usr/share/man/man1",
+        creates => "/usr/share/man/man1/er.1",
+        require => Exec["git clone ${perl_range_name}"];
+    }
+
+  }
+  else {
+    notify { "It's up to you to provde ${perl_range_name}": }
   }
 
 }
